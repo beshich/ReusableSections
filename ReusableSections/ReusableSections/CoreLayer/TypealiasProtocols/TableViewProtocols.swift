2f578f31702c6148d6a78c681bc287710a059b21
Agatay <beshssg@gmail.com>
2023-01-28 16:16:03 +0500

    feat (project): добавление typealias протокола для таблицы

---
+++ ReusableSections/ReusableSections/CoreLayer/TypealiasProtocols/TableViewProtocols.swift
@@ -0,0 +1,10 @@
+//
+//  TableViewProtocols.swift
+//  ReusableSections
+//
+//  Created by Agatay Embeev on 28.01.2023.
+//
+
+import UIKit
+
+public typealias TableViewDelegateAndDataSourcesProtocol = UITableViewDelegate & UITableViewDataSource
